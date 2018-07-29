516bdd92e9c28c5e166e13a90a82a5e032e00753
Tomislav Jurin <svezauzeto12@hotmail.com>
2018-07-29 14:04:35 +0200

    MVWB-172 cmd modified to load all jars from lib folder

--- MV.Wildebeest.Core/source/core/etc/bin/wb.cmd
+++ MV.Wildebeest.Core/source/core/etc/bin/wb.cmd
@@ -18,42 +18,15 @@ REM Wildebeest.  If not, see http://www.gnu.org/licenses/gpl-2.0.html
 
 if "%WB_HOME%"=="" goto noWbHome
 
-REM Wildebeest
-SET WB_CLASSPATH=%WB_HOME%\lib\MV.Wildebeest.Api-@meta.project.version.full.dotted@.jar
-SET WB_CLASSPATH=%WB_CLASSPATH%;%WB_HOME%\lib\MV.Wildebeest.Core-@meta.project.version.full.dotted@.jar
-
-REM Logging
-SET WB_CLASSPATH=%WB_CLASSPATH%;%WB_HOME%\lib\slf4j-api-1.7.7.jar
-SET WB_CLASSPATH=%WB_CLASSPATH%;%WB_HOME%\lib\slf4j-log4j12-1.7.7.jar
-SET WB_CLASSPATH=%WB_CLASSPATH%;%WB_HOME%\lib\log4j-1.2.17.jar
-
-REM Plugin Support
-SET WB_CLASSPATH=%WB_CLASSPATH%;%WB_HOME%\lib\mysql-connector-java-8.0.11.jar
-SET WB_CLASSPATH=%WB_CLASSPATH%;%WB_HOME%\lib\postgresql-9.3-1100-jdbc41.jar
-SET WB_CLASSPATH=%WB_CLASSPATH%;%WB_HOME%\lib\sqljdbc4.jar
-
-REM Other Libraries
-SET WB_CLASSPATH=%WB_CLASSPATH%;%WB_HOME%\lib\guava-20.0.jar
-SET WB_CLASSPATH=%WB_CLASSPATH%;%WB_HOME%\lib\javassist-3.21.0-GA.jar
-SET WB_CLASSPATH=%WB_CLASSPATH%;%WB_HOME%\lib\reflections-0.9.11.jar
-SET WB_CLASSPATH=%WB_CLASSPATH%;%WB_HOME%\lib\joda-time-2.10.jar
-SET WB_CLASSPATH=%WB_CLASSPATH%;%WB_HOME%\lib\java-cup-10k.jar
-SET WB_CLASSPATH=%WB_CLASSPATH%;%WB_HOME%\lib\org.eclipse.wst.xml.xpath2.processor-1.1.5-738bb7b85d.jar
-SET WB_CLASSPATH=%WB_CLASSPATH%;%WB_HOME%\lib\picocli-3.3.0.jar
-SET WB_CLASSPATH=%WB_CLASSPATH%;%WB_HOME%\lib\protobuf-java-2.6.0.jar
-SET WB_CLASSPATH=%WB_CLASSPATH%;%WB_HOME%\lib\xercesImpl-xsd11-2.12-beta-r1667115.jar
-SET WB_CLASSPATH=%WB_CLASSPATH%;%WB_HOME%\lib\xml-apis-1.4.01.jar
-
-
 if defined JAVA_HOME (
-  java -classpath %WB_CLASSPATH% co.mv.wb.cli.WildebeestCommand %*
+  java -classpath "%WB_HOME%\lib\*" co.mv.wb.cli.WildebeestCommand %*
   goto exit
 )
 
 if not defined JAVA_HOME (
   if exist "%WB_HOME%\jdk-10.0.2\bin\java.exe" (
     set JAVA_HOME="%WB_HOME%\jdk-10.0.2\"
-    %WB_HOME%\jdk-10.0.2\bin\java.exe -classpath %WB_CLASSPATH% co.mv.wb.cli.WildebeestCommand %*
+    %WB_HOME%\jdk-10.0.2\bin\java.exe -classpath "%WB_HOME%\lib\*" co.mv.wb.cli.WildebeestCommand %*
     goto exit
   )
   if not exist %WB_HOME%\openjdk-10.0.2\bin\java.exe (
